3c91062e6d4dfde1024357df8f329a2e95c8d299
snarksliveshere <snarksliveshere@gmail.com>
2019-01-19 15:01:27 +0300

    add imageModel

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
 
 class App extends StatefulWidget {
   createState() {
@@ -9,21 +10,25 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
 
+  void fetchImage() {
+            //  setState(() {
+            //     counter++;            
+            //   });
+
+  }
+
   Widget build(context) {
     return MaterialApp(
         home: Scaffold(
           body: Text('$counter'),
           appBar: AppBar(
-            title: Text('Lets see some images!'),
+            title: Text('Lets see some 1images!'),
           ),
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
-            onPressed: () { 
-              setState(() {
-                counter++;            
-              });
-            },
+            onPressed: this.fetchImage,
           ),
     ));
   }
 }
+ 
